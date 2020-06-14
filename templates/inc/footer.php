9c42066cd4b2c8c9ace89fee678519c5fcfdc44c
JustCabyr <akasu@dervac.com>
2020-06-14 09:27:17 +0100

    initial upload

---
+++ templates/inc/footer.php
@@ -0,0 +1,7 @@
+			<footer class="footer">
+		<p>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved, Recruiter Inc.</p>
+      </footer>
+
+    </div>
+		</body>
+</html>
